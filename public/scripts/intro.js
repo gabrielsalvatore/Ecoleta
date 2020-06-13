8af56112fe793f1ed52cf669a02742ed6c4ee516
Gabriel Salvatore <you@example.com>
2020-06-12 21:27:40 -0300

    Projeto Ecoleta

---
+++ public/scripts/intro.js
@@ -0,0 +1,66 @@
+//document.write("Hello")
+
+
+// variaveis, tipos de dados
+//var myvar = "Hello"
+//const myconst = "Hello"
+
+//document.write(myconst + myvar)
+
+// string
+// number
+//const n1 = 12534097534965934657846
+//const n2 = 10000000000000000000
+//document.write(n1 + n2)
+
+//boolean - true or false
+//const bTrue = true
+//const bFalse = false
+//document.write(bFalse)
+
+// objeto possuem propiedades e funcionalidades
+//const pessoa = {
+  // altura: "1,93m",
+  // idade: 24,
+  // solteiro: true,
+ // correr(){
+   // return "run Forest"
+ 
+
+//document.write(pessoa.correr())
+
+// Array - Vetores
+//const colecaoDeBolinhas = ["blue", "green", 1, {name: "GSX NTC"}]
+
+//document.write(colecaoDeBolinhas[3].name)
+
+
+//Funções - Funcionalidades - Atalhos - Reuso do código
+
+//registrar função
+//function sayMyName(name) {
+  
+ // document.write(name)
+// }
+
+// executar
+//sayMyName("Enrique")
+//sayMyName("Silver")
+
+
+
+// condicionais
+
+//const notaFinal = 7
+
+//if( notaFinal < 5 ) {
+//  document.write("Reprovado")
+//} else{
+//  document.write("Aprovado")
+// }
+
+// loop repetições
+
+//for (i = 0; i < 10; i++) {
+  //document.write(`<p>${i}</p>`)
+//}
